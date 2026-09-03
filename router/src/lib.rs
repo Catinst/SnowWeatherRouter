7a51ba1d87d28d3326bae987217640e52775c353
Snownight <Catinst@users.noreply.github.com>
2026-09-03 22:04:50 +0800

    Defer engine initialization until activity resume

--- router/src/lib.rs
+++ router/src/lib.rs
@@ -1379,6 +1379,5 @@ pub unsafe extern "C" fn ANativeActivity_onCreate(
         (*callbacks).on_input_queue_destroyed = Some(on_input_queue_destroyed);
     }
 
-    log_static(ANDROID_LOG_INFO, b"SnowWeatherRouter onCreate\0");
-    initialize_engine();
+    log_static(ANDROID_LOG_INFO, b"SnowWeatherRouter onCreate; engine deferred to resume\0");
 }
